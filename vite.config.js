17db5f9a2e9eb7379c45d9819bb2a4dc5811d575
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-08-10 16:33:20 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Replace 'dist' with 'build' if you want to use "build" as the output directory.
+  },
+});
